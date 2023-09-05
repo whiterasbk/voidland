37e6bd8e6840f81239c70b54f9e931e587bc9df6
whiterasbk <whiterasbk@qq.com>
2023-09-05 14:36:34 +0800

    create sample block & item

--- src/main/java/top/whiterasbk/mcmod/fabric/voidland/VoidLandMain.java
+++ src/main/java/top/whiterasbk/mcmod/fabric/voidland/VoidLandMain.java
@@ -4,23 +4,44 @@ import com.mojang.logging.LogUtils;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.Block;
+import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
 import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
+import top.whiterasbk.mcmod.fabric.voidland.block.SampleBlock;
+import top.whiterasbk.mcmod.fabric.voidland.item.SampleItem;
 
 import static net.minecraft.server.command.CommandManager.literal;
 
 public class VoidLandMain implements ModInitializer {
 
     private static final Logger logger = LogUtils.getLogger();
-
-    public static final Item VOIDLAND_ITEM = new Item(new FabricItemSettings());
+    public static final String modid = "voidland";
+    public static final String namespace = modid;
 
     /**
      * Runs the mod initializer.
      */
     @Override
     public void onInitialize() {
+
+        // registering items
+        final Item SAMPLE_ITEM =
+                Registry.register(Registries.ITEM, new Identifier(namespace, "a_voidland_sample_item"),
+                        new SampleItem(new FabricItemSettings()));
+
+        // registering blocks
+        final Block SAMPLE_BLOCK = Registry.register(Registries.BLOCK, new Identifier(namespace, "a_voidland_sample_block"),
+                new SampleBlock(FabricBlockSettings.create().strength(4.0f)));
+        Registry.register(Registries.ITEM, new Identifier(namespace, "a_voidland_sample_block"),
+                new BlockItem(SAMPLE_BLOCK, new FabricItemSettings()));
+
+
 
         CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) ->
             dispatcher.register(literal("voidland")
@@ -30,6 +51,9 @@ public class VoidLandMain implements ModInitializer {
                 })
             )
         );
+
+
+
     }
 }
 
---
+++ src/main/java/top/whiterasbk/mcmod/fabric/voidland/block/SampleBlock.java
@@ -0,0 +1,25 @@
+package top.whiterasbk.mcmod.fabric.voidland.block;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.text.Text;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
+public class SampleBlock extends Block {
+    public SampleBlock(Settings settings) {
+        super(settings);
+    }
+
+    public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
+        if (!world.isClient) {
+            player.sendMessage(Text.of("Hello, world!"), false);
+        }
+
+        return ActionResult.SUCCESS;
+    }
+}
